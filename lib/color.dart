613999fe2462e396f5ad46e85ecb9d4eb7ca4a15
Bogdan Serban <106107980+SVBogdan@users.noreply.github.com>
2022-06-20 14:26:28 +0300

    Create color.dart

---
+++ lib/color.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColors {
+  static final Color mainColor = Color.fromARGB(255, 98, 43, 138);
+  static final Color myOffWhite = Color.fromARGB(255, 255, 252, 241);
+  static final Color greyPurple = Color.fromARGB(255, 130, 109, 140);
+  static final Color greyPurpleUnread = Color.fromARGB(16, 98, 43, 138);
+  static final Color myOffWhiteDark = Color.fromARGB(255, 246, 241, 224);
+}
